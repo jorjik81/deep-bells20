4c8004a0c0b27f863c7c594170406bbcddc18309
Gio <jorjik81@gmail.com>
2024-01-15 23:35:48 +0400

    images linked according react rules

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,38 +1,55 @@
-import React from 'react';
+
+import projectZeroImage from "../../assets/images/photo (2).jpg";
+import projectOneImage from "../../assets/images/tape.jpg";
+import projectTwoImage from "../../assets/images/333.jpg";
+import projectThreeImage from "../../assets/images/neuron.jpg";
+import projectFourImage from "../../assets/images/view.webp";
+
 
 const Projects = () => {
   return (
     <div>
       <h1>Projects</h1>
       <div className="content">
-        <a target="_blank" href="https://drive.google.com/drive/folders/0B8yFFDpT3LJVRV9oTk0wWTN1dDQ?resourcekey=0-MLqd64Yc5aFNXoUxYF322Q&usp=sharing">
+        <a target="_blank">
           <div className="firstImg">
-            <img src="./src/assets/images/photo (2).jpg" alt="Project zero" />
-            <figcaption className="image-caption">Photography Works</figcaption>
+            <img src={projectZeroImage} alt="Project zero" />
           </div>
         </a>
         <div className="grid">
           <a href="https://www.youtube.com/@Giorgijorjadze/featured">
             <div className="smallImg">
-              <img src="./src/assets/images/tape.jpg" alt="project one" />
+              <img src={projectOneImage} alt="project one" />
               <figcaption className="image-caption">My Youtube Channel</figcaption>
             </div>
           </a>
           <a href="https://www.linkedin.com/in/giorgi-jorjadze-7189a862/">
             <div className="smallImg">
-              <img src="./src/assets/images/333.jpg" alt="project two" />
+              <img src={projectTwoImage} alt="project two" />
               <figcaption className="image-caption">My LinkedIn</figcaption>
             </div>
           </a>
           <a href="https://github.com/jorjik81">
             <div className="smallImg">
-              <img src="./src/assets/images/neuron.jpg" alt="project three" />
+              <img src={projectThreeImage} alt="project three" />
               <figcaption className="image-caption">My GitHub</figcaption>
             </div>
           </a>
           <a href="https://github.com/BrandonERivera/frontendprobject-1">
             <div className="smallImg">
-              <img src="./src/assets/images/view.webp" alt="project four" />
+              <img src={projectFourImage} alt="project four" />
+              <figcaption className="image-caption">My Project</figcaption>
+            </div>
+          </a>
+          <a href="https://github.com/jorjik81">
+            <div className="smallImg">
+              <img src={projectThreeImage} alt="project three" />
+              <figcaption className="image-caption">My GitHub</figcaption>
+            </div>
+          </a>
+          <a href="https://github.com/BrandonERivera/frontendprobject-1">
+            <div className="smallImg">
+              <img src={projectFourImage} alt="project four" />
               <figcaption className="image-caption">My Project</figcaption>
             </div>
           </a>
